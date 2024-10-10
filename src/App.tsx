a451d51c2b4f586ea4f2a625e91f7d1d1005f6cc
Shivam Bansal <shivambansal@Shivams-MacBook-Air.local>
2024-10-10 23:19:08 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+//@ts-ignore
+
+import Counter from './counter'
+import './App.css'
+// import {Blog} from './blog'
+function App() {
+
+  return (
+    <div className="App">
+    <h1>Hello</h1>
+    <Counter/>
+      
+   {/* <div className="navbar">
+    <ul>
+     <li>Home</li>
+     <li>Blog</li>
+    </ul>
+   </div> */}
+  </div>
+  
+  )
+}
+
+export default App
